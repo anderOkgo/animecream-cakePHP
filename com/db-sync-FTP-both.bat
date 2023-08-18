c462cedca13c0d5e753f7fee423dab3397a24d01
anderson rangel <andergroundmsg@gmail.com>
2023-08-18 21:08:54 +1000

    sync refactorin code

--- com/db-sync-FTP-both.bat
+++ com/db-sync-FTP-both.bat
@@ -1,13 +1,14 @@
 @ECHO Off
 SETLOCAL
 IF EXIST setter.bat (CD.. & CALL com\setter.bat) ELSE ( CALL com\setter.bat )
-start "" %Link_restart_node.txt%
 "keys/WinSCP.com" ^
   /command ^
     "open ftp://%FTPUsername.txt%:%FTPPss.txt%@%FTPserver.txt%" ^
     "option batch off" ^
-    "synchronize remote ""%LocalPath.txt%\dist"" /%FTPFolder.txt%" ^
-    "put "%LocalPath.txt%\package.json" /package.json" ^
+    "synchronize remote ""%LocalPath.txt%\webroot\img\tarjeta\"" /webroot/img/tarjeta/" ^
+    "synchronize local ""%LocalPath.txt%\config\"" /config/" ^
+    "synchronize local ""%LocalPath.txt%\src\"" /src/" ^
+    "synchronize local ""%LocalPath.txt%\webroot\"" /webroot/" ^
     "exit" ^
 IF %ERRORLEVEL% EQU 0 (Echo No error found) ELSE (Echo An error was found)
 PAUSE
