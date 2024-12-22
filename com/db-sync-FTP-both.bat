82841d1d18b98e420517b0d32f34373e71d1b6f0
anderson rangel <andergroundmsg@gmail.com>
2024-12-21 22:44:43 -0500

    sycv remote set

--- com/db-sync-FTP-both.bat
+++ com/db-sync-FTP-both.bat
@@ -7,8 +7,8 @@ IF EXIST setter.bat (CD.. & CALL com\setter.bat) ELSE ( CALL com\setter.bat )
     "option batch off" ^
     "synchronize remote ""%LocalPath.txt%\webroot\img\tarjeta\"" /webroot/img/tarjeta/" ^
     "synchronize local ""%LocalPath.txt%\config\"" /config/" ^
-    "synchronize both ""%LocalPath.txt%\src\"" /src/" ^
-    "synchronize both ""%LocalPath.txt%\webroot\"" /webroot/" ^
+    "synchronize remote ""%LocalPath.txt%\src\"" /src/" ^
+    "synchronize remote ""%LocalPath.txt%\webroot\"" /webroot/" ^
     "exit" ^
 IF %ERRORLEVEL% EQU 0 (Echo No error found) ELSE (Echo An error was found)
 PAUSE
