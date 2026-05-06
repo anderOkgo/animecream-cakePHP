fa5182141040270fa37d989f6d100d9651e5d978
anderson3as <anderson@tresastronautas.com>
2026-05-05 22:14:38 -0500

    feat: add batch script to synchronize local and FTP image directories using WinSCP

--- com/db-sync-FTP-both.bat
+++ com/db-sync-FTP-both.bat
@@ -5,10 +5,7 @@ IF EXIST setter.bat (CD.. & CALL com\setter.bat) ELSE ( CALL com\setter.bat )
   /command ^
     "open ftp://%FTPUsername.txt%:%FTPPss.txt%@%FTPserver.txt%" ^
     "option batch off" ^
-    "synchronize both ""%LocalPath.txt%\webroot\img\tarjeta\"" /webroot/img/tarjeta/" ^
-    "synchronize local ""%LocalPath.txt%\config\"" /config/" ^
-    "synchronize remote ""%LocalPath.txt%\src\"" /src/" ^
-    "synchronize remote ""%LocalPath.txt%\webroot\"" /webroot/" ^
+    "synchronize both ""%LocalPath.txt%\webroot\img\tarjeta\"" /img/tarjeta/" ^
     "exit" ^
 IF %ERRORLEVEL% EQU 0 (Echo No error found) ELSE (Echo An error was found)
 PAUSE
